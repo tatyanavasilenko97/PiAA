ad40b841432b0cd8ac5e8e84a10058c4731b0357
HECTOPK <hectopk@gmail.com>
2018-04-17 20:11:56 +0300

    Add files via upload

--- 6381/PolyakovNS/lab2/greedy/lab2.cpp
+++ 6381/PolyakovNS/lab2/greedy/lab2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Edge.hpp"
 #include "Vertex.hpp"
 #include "Graph.hpp"
@@ -6,11 +7,13 @@
 int main(){
     Graph my_graph;
     char start, end;
-    std::cin >> start >> end;
+    std::ifstream file;
+    file.open("input.txt",std::ios::in);
+    file >> start >> end;
     char edge_start;
     char edge_end;
     double weight;
-    while(std::cin >> edge_start >> edge_end >> weight){
+    while(file >> edge_start >> edge_end >> weight){
         my_graph.addEdge(edge_start, edge_end, weight);
     }
     std::cout << my_graph.greedy(start, end) << std::endl;
