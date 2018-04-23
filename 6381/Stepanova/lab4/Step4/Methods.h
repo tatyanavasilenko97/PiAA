29d82e8771ec65cb6454356179e61ffa39c38436
Stepanova Sofya <stesoi@mail.ru>
2018-04-23 12:11:55 +0300

    Add files via upload

---
+++ 6381/Stepanova/lab4/Step4/Methods.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+
+void PrefixFunction(std::string& Common, std::vector<unsigned long long int>& Pi);//префикс-функция
+void AlgorithmKMP(std::string& P, std::string& T);//алгоритм Кнута-Морриса-Пратта
