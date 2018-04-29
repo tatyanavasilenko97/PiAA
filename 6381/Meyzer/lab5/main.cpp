57a9926987a6d926210d1f3efae99c2676272bdf
Denmey <Denmey98@gmail.com>
2018-04-29 20:05:06 +0300

    Added stepik's test and input processing function

--- 6381/Meyzer/lab5/main.cpp
+++ 6381/Meyzer/lab5/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+void readData(string &Text, unsigned& PattN, vector<string> &Patterns)
+{
+	cin >> Text >> PattN;
+	string Pattern;
+	for (unsigned i = 0; i < PattN; ++i)
+	{
+		cin >> Pattern;
+		Patterns.push_back(Pattern);
+	}
+}
 
 int main()
 {
-	
+	string Text;
+	unsigned PattN = 0;
+	vector<string> Patterns;
+	readData(Text, PattN, Patterns);
+
+	return 0;
 }
