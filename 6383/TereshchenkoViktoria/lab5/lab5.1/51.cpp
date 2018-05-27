0231cedf13a16c8895603818bf6bc582fa1111be
vika210899 <vika210899@mail.ru>
2018-05-27 15:48:19 +0300

    6383 TereshchenkoVN lab5

---
+++ 6383/TereshchenkoViktoria/lab5/lab5.1/51.cpp
@@ -0,0 +1,109 @@
+//lab5_1
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#define alph 52
+
+using namespace std;
+
+struct Bohr {
+	int next_vrtx[alph];
+	int pattern_num; 
+	bool flag; 
+	int suff_link; 
+	int auto_move[alph]; 
+	int par; 
+	char symb; 
+
+};
+
+vector<Bohr> bohr;
+vector<string> pattern; 
+
+Bohr makeBohr(int p, char c) { 
+	Bohr v; 
+	memset(v.next_vrtx, -1, sizeof(v.next_vrtx)); 
+	v.flag = false; 
+	v.suff_link = -1; 
+	memset(v.auto_move, -1, sizeof(v.auto_move)); 
+	v.par = p;
+	v.symb = c;
+	v.suff_flink = -1;
+	return v;
+}
+
+void initBohr() {
+	bohr.push_back(makeBohr(0, -1));
+}
+
+void addPatternToBohr(const string &s) {
+	int num = 0; 
+	for (int i = 0; i < s.length(); i++) {
+		char ch = s[i] - 'A'; 
+		if (bohr[num].next_vrtx[ch] == -1) { 
+			bohr.push_back(makeBohr(num, ch));
+			bohr[num].next_vrtx[ch] = bohr.size() - 1;
+		}
+		num = bohr[num].next_vrtx[ch]; 
+	}
+	bohr[num].flag = true; 
+	pattern.push_back(s); 
+	bohr[num].pattern_num = pattern.size() - 1; 
+}
+
+int getAutoMove(int v, char ch);
+
+int getSuffLink(int v) { 
+	if (bohr[v].suff_link == -1)
+		if (v == 0 || bohr[v].par == 0)
+			bohr[v].suff_link = 0; 
+		else
+			bohr[v].suff_link = getAutoMove(getSuffLink(bohr[v].par), bohr[v].symb); 
+	return bohr[v].suff_link;
+}
+
+int getAutoMove(int v, char ch) {
+	if (bohr[v].auto_move[ch] == -1) 
+		if (bohr[v].next_vrtx[ch] != -1)
+			bohr[v].auto_move[ch] = bohr[v].next_vrtx[ch];
+		else
+			if (v == 0)
+				bohr[v].auto_move[ch] = 0; 
+			else
+				bohr[v].auto_move[ch] = getAutoMove(getSuffLink(v), ch);
+	return bohr[v].auto_move[ch];
+}
+
+void check(int v, int i) {
+	for (int u = v; u != 0; u = getSuffLink(u)) {
+		if (bohr[u].flag) 
+			cout << i - pattern[bohr[u].pattern_num].length() + 1 << " " << bohr[u].pattern_num + 1 << endl;
+	}
+}
+
+void findAllPos(const string& s) { 
+	int u = 0;
+	for (int i = 0; i < s.length(); i++) {
+		u = getAutoMove(u, s[i] - 'A'); 
+		check(u, i + 1); 
+	}
+}
+
+int main()
+{
+	initBohr(); 
+	string text; 
+	string pattern;
+	int n; 
+	cin >> text >> n;
+
+	for (int i = 0; i < n; i++) { 
+		cin >> pattern;
+		addPatternToBohr(pattern);
+	}
+
+	findAllPos(text); 
+	return 0;
+}
