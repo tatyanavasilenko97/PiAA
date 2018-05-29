4f7577d4a092cfbc60098a8c76e270e1dc5945f4
yar1k112 <yarik1998@gmail.com>
2018-05-30 00:00:13 +0300

    fix

--- 6381/Shiryaev/lab4/lab4_2.cpp
+++ 6381/Shiryaev/lab4/lab4_2.cpp
@@ -1,43 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cstddef>
+#include <iostream> 
+#include <string> 
+#include <vector> 
 
 using namespace std;
 
-vector<size_t> prefix_function (string s);
+vector<size_t> prefix_function(const string& pattern) { 
+	vector<size_t> pi (pattern.length()); 
+	pi[0] = 0; 
+	for (size_t k = 0, i = 1; i < pattern.length(); ++i) { 
 
-int main(){
-	string text;
-	string tmplt;
-	const char *c = "@"; 
-	int n = 0;
-	vector<size_t> kmp;
-	vector<size_t> res;
-	cin >> text;
-	cin >> tmplt;
-	n = tmplt.size();
-	text = tmplt + c + text;
-	kmp = prefix_function(text);
-	if(kmp.back() > 0 && kmp.back() < n)
-		cout << n - kmp.back();
-	else
-		cout << -1;
-}
+		while ((k > 0) && (pattern[i] != pattern[k]))	k = pi[k-1]; 
 
-vector<size_t> prefix_function (string s) 
-{
-    size_t n =  s.length();
-    vector<size_t> pi(n); // в i-м элементе (его индекс i-1) количество совпавших символов в начале и конце для построки длины i. 
-			 // p[0]=0 всегда, p[1]=1, если начинается с двух одинаковых 
-    for (size_t i=1; i<n; ++i) 
-    {
-        size_t j = pi[i-1];
-        while ((j > 0) && (s[i] != s[j])) // не равны
-            j = pi[j-1];				// берем ранее рассчитанное значение (начиная с максимально возможных)
-        if (s[i] == s[j]) // равны 
-            ++j; 
-        pi[i] = j;
-     }
-     return pi;
+		if (pattern[i] == pattern[k])	k++; 
+
+		pi[i] = k; 
+	} 
+	return pi; 
+} 
+
+int KMP(const string& str, const string& pattern){ 
+	vector<size_t> pi = prefix_function(pattern);
+
+	for (size_t k = 0, i = 0; i < str.length(); ++i){ 
+		while ((k > 0) && (pattern[k] != str[i]))		k = pi[k-1]; 
+
+		if (pattern[k] == str[i])		k++;
+
+		if (k == pattern.length())	return (i - pattern.length() + 1); 
+	} 
+	return -1; 
+} 
+
+int main()
+ { 
+	string A, B; 
+	cin >> A; 
+	cin >> B; 
+	cout << KMP(A + A, B) << endl; 
+	return 0; 
 }
